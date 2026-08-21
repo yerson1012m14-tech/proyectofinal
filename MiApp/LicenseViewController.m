7e2a4b9063dffd19220787c09bcd250bf47f4992
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 06:34:56 -0500

    Implement settings button and user preferences management
    Added settings button and functionality to load and save user settings, including language and screen protection preferences.

--- MiApp/LicenseViewController.m
+++ MiApp/LicenseViewController.m
@@ -1,17 +1,23 @@
 #import "LicenseViewController.h"
+#import "SettingsViewController.h"
+#import "Translations.h"
 #import <QuartzCore/QuartzCore.h>
 
 @interface LicenseViewController () <UITextFieldDelegate>
 @property (nonatomic, strong) UITextField *licenseField;
 @property (nonatomic, strong) UIButton *continueButton;
 @property (nonatomic, strong) CAGradientLayer *buttonGradient;
+@property (nonatomic, strong) UIButton *settingsButton;
+@property (nonatomic, assign) NSInteger selectedLanguage;
+@property (nonatomic, assign) BOOL screenProtection;
 @end
 
 @implementation LicenseViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor colorWithRed:0.018 green:0.018 blue:0.022 alpha:1.0];
+    [self loadSettings];
+    [Translations setLanguage:self.selectedLanguage];
     [self setupUI];
 }
 
@@ -22,6 +28,8 @@
     UIColor *fieldBorder = [UIColor colorWithWhite:0.20 alpha:1.0];
     UIColor *fieldFill = [UIColor colorWithWhite:0.065 alpha:1.0];
     UIColor *red = [UIColor colorWithRed:0.95 green:0.08 blue:0.10 alpha:1.0];
+    
+    self.view.backgroundColor = [UIColor blackColor];
     
     UIView *topGlow = [[UIView alloc] init];
     topGlow.translatesAutoresizingMaskIntoConstraints = NO;
@@ -90,11 +98,16 @@
     
     self.continueButton = [UIButton buttonWithType:UIButtonTypeCustom];
     self.continueButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.continueButton setTitle:@"CONTINUAR" forState:UIControlStateNormal];
-    [self.continueButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    self.continueButton.titleLabel.font = [UIFont systemFontOfSize:15.0 weight:UIFontWeightBold];
     self.continueButton.layer.cornerRadius = 14.0;
     self.continueButton.layer.masksToBounds = YES;
+    
+    [self.continueButton setAttributedTitle:[[NSAttributedString alloc] initWithString:[Translations tr:@"continue"]
+        attributes:@{
+            NSFontAttributeName: [UIFont systemFontOfSize:15.0 weight:UIFontWeightBold],
+            NSForegroundColorAttributeName: [UIColor whiteColor],
+            NSKernAttributeName: @1.5
+        }] forState:UIControlStateNormal];
+    
     [self.continueButton addTarget:self action:@selector(activateLicense) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.continueButton];
     
@@ -118,6 +131,15 @@
     buttonShadow.layer.shadowOffset = CGSizeZero;
     buttonShadow.userInteractionEnabled = NO;
     [self.view insertSubview:buttonShadow belowSubview:self.continueButton];
+    
+    // BOTÓN DE CONFIGURACIÓN
+    self.settingsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.settingsButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.settingsButton setImage:[UIImage systemImageNamed:@"gearshape.fill"] forState:UIControlStateNormal];
+    [self.settingsButton setTintColor:white];
+    self.settingsButton.contentMode = UIViewContentModeScaleAspectFit;
+    [self.settingsButton addTarget:self action:@selector(showSettings) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.settingsButton];
     
     [NSLayoutConstraint activateConstraints:@[
         [topGlow.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
@@ -157,6 +179,11 @@
         [self.continueButton.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:38],
         [self.continueButton.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-38],
         [self.continueButton.heightAnchor constraintEqualToConstant:54],
+        
+        [self.settingsButton.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:16],
+        [self.settingsButton.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20],
+        [self.settingsButton.widthAnchor constraintEqualToConstant:28],
+        [self.settingsButton.heightAnchor constraintEqualToConstant:28],
     ]];
     
     [self updateButtonGradientFrame];
@@ -170,6 +197,54 @@
 - (void)updateButtonGradientFrame {
     self.buttonGradient.frame = self.continueButton.bounds;
     self.buttonGradient.cornerRadius = self.continueButton.layer.cornerRadius;
+}
+
+#pragma mark - Settings
+
+- (void)loadSettings {
+    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+    self.selectedLanguage = [d integerForKey:@"selectedLanguage"];
+    self.screenProtection = [d boolForKey:@"screenProtection"];
+}
+
+- (void)saveSettings {
+    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+    [d setInteger:self.selectedLanguage forKey:@"selectedLanguage"];
+    [d setBool:self.screenProtection forKey:@"screenProtection"];
+    [d synchronize];
+}
+
+- (void)showSettings {
+    SettingsViewController *svc = [[SettingsViewController alloc] init];
+    svc.selectedLanguage = self.selectedLanguage;
+    svc.screenProtection = self.screenProtection;
+    
+    __weak typeof(self) weakSelf = self;
+    svc.onSettingsChanged = ^{
+        if (!weakSelf) return;
+        weakSelf.selectedLanguage = svc.selectedLanguage;
+        weakSelf.screenProtection = svc.screenProtection;
+        [weakSelf saveSettings];
+        [Translations setLanguage:weakSelf.selectedLanguage];
+        [weakSelf updateButtonText];
+    };
+    
+    svc.modalPresentationStyle = UIModalPresentationPageSheet;
+    if (@available(iOS 15.0, *)) {
+        UISheetPresentationController *sheet = svc.sheetPresentationController;
+        sheet.detents = @[UISheetPresentationControllerDetent.mediumDetent, UISheetPresentationControllerDetent.largeDetent];
+        sheet.preferredCornerRadius = 20;
+    }
+    [self presentViewController:svc animated:YES completion:nil];
+}
+
+- (void)updateButtonText {
+    [self.continueButton setAttributedTitle:[[NSAttributedString alloc] initWithString:[Translations tr:@"continue"]
+        attributes:@{
+            NSFontAttributeName: [UIFont systemFontOfSize:15.0 weight:UIFontWeightBold],
+            NSForegroundColorAttributeName: [UIColor whiteColor],
+            NSKernAttributeName: @1.5
+        }] forState:UIControlStateNormal];
 }
 
 #pragma mark - License formatting
@@ -218,10 +293,10 @@
             self.onLicenseValidated();
         }
     } else {
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Clave no válida"
-            message:@"Ingresa una clave con el formato XXXX-XXXX-XXXX-XXXX."
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:[Translations tr:@"invalid_key"]
+            message:[Translations tr:@"key_format"]
             preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"Reintentar" style:UIAlertActionStyleDefault handler:nil]];
+        [alert addAction:[UIAlertAction actionWithTitle:[Translations tr:@"retry"] style:UIAlertActionStyleDefault handler:nil]];
         [self presentViewController:alert animated:YES completion:nil];
     }
 }
