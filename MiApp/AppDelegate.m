e0281546009cb4ac7619d5bdfd180a4c0da0c5be
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 05:54:15 -0500

    Refactor AppDelegate by removing protection features
    Removed screen protection functionality and related methods.

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -4,9 +4,6 @@
 
 @interface AppDelegate ()
 @property (nonatomic, strong) UIWindow *lockWindow;
-@property (nonatomic, strong) UIView *protectionView;
-@property (nonatomic, assign) BOOL isRecording;
-@property (nonatomic, assign) BOOL protectionEnabled;
 @end
 
 @implementation AppDelegate
@@ -31,140 +28,10 @@
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     
-    // Cargar configuración
-    self.protectionEnabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"screenProtection"];
-    
-    if (self.protectionEnabled) {
-        [self setupProtection];
-    }
-    
-    // Escuchar cambios
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(protectionSettingChanged)
-                                                 name:@"ProtectionSettingChanged"
-                                               object:nil];
-    
     [self mostrarPantallaLicencia];
     
     return YES;
 }
-
-- (void)setupProtection {
-    // Vista negra que se pone encima cuando hay grabación
-    self.protectionView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.protectionView.backgroundColor = [UIColor blackColor];
-    self.protectionView.alpha = 0.0;
-    self.protectionView.tag = 9999;
-    self.protectionView.userInteractionEnabled = NO;
-    [self.window addSubview:self.protectionView];
-    [self.window bringSubviewToFront:self.protectionView];
-    
-    // 1. Detectar grabación de pantalla
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(screenCaptureDidChange)
-                                                 name:UIScreen.capturedDidChangeNotification
-                                               object:nil];
-    
-    // 2. Detectar captura de pantalla
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(userDidTakeScreenshot)
-                                                 name:UIApplicationUserDidTakeScreenshotNotification
-                                               object:nil];
-    
-    // 3. Cambio de app / multitarea
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(appWillResignActive)
-                                                 name:UIApplicationWillResignActiveNotification
-                                               object:nil];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(appDidBecomeActive)
-                                                 name:UIApplicationDidBecomeActiveNotification
-                                               object:nil];
-    
-    // Verificar si ya está grabando
-    self.isRecording = [UIScreen mainScreen].isCaptured;
-    if (self.isRecording) {
-        [self showProtection];
-    }
-}
-
-#pragma mark - Grabación
-
-- (void)screenCaptureDidChange {
-    BOOL isCaptured = [UIScreen mainScreen].isCaptured;
-    
-    if (isCaptured && !self.isRecording) {
-        self.isRecording = YES;
-        [self showProtection];
-    } else if (!isCaptured && self.isRecording) {
-        self.isRecording = NO;
-        if (![self isAppInBackground]) {
-            [self hideProtection];
-        }
-    }
-}
-
-#pragma mark - Captura
-
-- (void)userDidTakeScreenshot {
-    // Flash negro rápido para que la siguiente captura (si es rápida) salga negra
-    [self showProtection];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        if (!self.isRecording && ![self isAppInBackground]) {
-            [self hideProtection];
-        }
-    });
-}
-
-#pragma mark - Cambio de app
-
-- (void)appWillResignActive {
-    [self showProtection];
-}
-
-- (void)appDidBecomeActive {
-    if (!self.isRecording) {
-        [self hideProtection];
-    }
-}
-
-- (BOOL)isAppInBackground {
-    UIApplication *app = [UIApplication sharedApplication];
-    return app.applicationState != UIApplicationStateActive;
-}
-
-#pragma mark - Mostrar/Ocultar
-
-- (void)showProtection {
-    [UIView animateWithDuration:0.15 animations:^{
-        self.protectionView.alpha = 1.0;
-    }];
-}
-
-- (void)hideProtection {
-    [UIView animateWithDuration:0.15 animations:^{
-        self.protectionView.alpha = 0.0;
-    }];
-}
-
-- (void)protectionSettingChanged {
-    BOOL enabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"screenProtection"];
-    
-    if (enabled && !self.protectionEnabled) {
-        self.protectionEnabled = YES;
-        [self setupProtection];
-    } else if (!enabled && self.protectionEnabled) {
-        self.protectionEnabled = NO;
-        [self hideProtection];
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIScreen.capturedDidChangeNotification object:nil];
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationUserDidTakeScreenshotNotification object:nil];
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillResignActiveNotification object:nil];
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
-    }
-}
-
-#pragma mark - Licencia
 
 - (void)mostrarPantallaLicencia {
     NSString *licenciaGuardada = [[NSUserDefaults standardUserDefaults] stringForKey:@"MiFilzaLicenseKey"];
