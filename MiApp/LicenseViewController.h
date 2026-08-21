8e4d54399a3416d2d260c75f6116244cfc5776a9
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 03:24:25 -0500

    Add nullability annotations to LicenseViewController

--- MiApp/LicenseViewController.h
+++ MiApp/LicenseViewController.h
@@ -1,7 +1,11 @@
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface LicenseViewController : UIViewController
 
 @property (nonatomic, copy) void (^onLicenseValidated)(void);
 
 @end
+
+NS_ASSUME_NONNULL_END
