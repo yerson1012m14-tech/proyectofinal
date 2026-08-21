b909913bb62c3a68430b3c495762cf369502062b
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 01:48:55 -0500

    Modify AppDelegate for status bar style handling
    Updated AppDelegate to set status bar style for iOS 13 and later.

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -1,29 +1,25 @@
+//
+//  AppDelegate.m
+//  MiApp
+//
+
 #import "AppDelegate.h"
 #import "ViewController.h"
+
+@interface AppDelegate ()
+
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    UIColor *acento = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
-
-    UINavigationBarAppearance *ap = [[UINavigationBarAppearance alloc] init];
-    [ap configureWithOpaqueBackground];
-    ap.backgroundColor = [UIColor blackColor];
-    ap.shadowColor = [UIColor colorWithWhite:0.25 alpha:1.0];
-    ap.titleTextAttributes = @{
-        NSForegroundColorAttributeName: acento,
-        NSFontAttributeName: [UIFont fontWithName:@"Menlo-Bold" size:17]
-    };
-    [[UINavigationBar appearance] setStandardAppearance:ap];
-    [[UINavigationBar appearance] setScrollEdgeAppearance:ap];
-    [[UINavigationBar appearance] setTintColor:acento];
-
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    ViewController *vc = [[ViewController alloc] init];
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
-    self.window.rootViewController = nav;
-    [self.window makeKeyAndVisible];
+    // Estilo de la barra de estado
+    if (@available(iOS 13.0, *)) {
+        UIWindowScene *scene = (UIWindowScene *)application.windows.firstObject.windowScene;
+        scene.statusBarStyle = UIStatusBarStyleLightContent;
+    }
     return YES;
 }
 
+// ... resto igual
 @end
