1899d1af4076d0b9fdf498015cef26db7f60c0d7
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 11:34:45 -0500

    Add method declaration for translation retrieval

--- MiApp/Translations.h
+++ MiApp/Translations.h
@@ -1,9 +1,11 @@
 #import <Foundation/Foundation.h>
 
+extern NSString * const TranslationsLanguageDidChangeNotification;
+
 @interface Translations : NSObject
 
-+ (NSString *)tr:(NSString *)key;
 + (void)setLanguage:(NSInteger)language;
 + (NSInteger)currentLanguage;
++ (NSString *)tr:(NSString *)key;
 
 @end
