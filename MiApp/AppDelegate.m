0ba126f42be747d188bb3f62a754b35fd2e482ab
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 05:09:14 -0500

    Refactor AppDelegate for protection and license management
    Refactor AppDelegate to improve protection handling and license validation.

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -6,6 +6,7 @@
 @property (nonatomic, strong) UIWindow *lockWindow;
 @property (nonatomic, strong) UIView *protectionView;
 @property (nonatomic, assign) BOOL isRecording;
+@property (nonatomic, assign) BOOL protectionEnabled;
 @end
 
 @implementation AppDelegate
@@ -30,9 +31,10 @@
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     
-    // Verificar si la protección está activada
-    BOOL protectionEnabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"screenProtection"];
-    if (protectionEnabled) {
+    // Cargar configuración de protección
+    self.protectionEnabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"screenProtection"];
+    
+    if (self.protectionEnabled) {
         [self setupProtection];
     }
     
@@ -43,13 +45,27 @@
 
 - (void)setupProtection {
     // Crear la vista de protección (pantalla negra)
+    // Esta vista SIEMPRE está presente pero invisible normalmente
     self.protectionView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.protectionView.backgroundColor = [UIColor blackColor];
     self.protectionView.alpha = 0.0;
     self.protectionView.tag = 9999;
+    self.protectionView.userInteractionEnabled = NO;
     [self.window addSubview:self.protectionView];
     
-    // 1. Protección al cambiar de app / multitarea
+    // 1. Protección contra grabación de pantalla
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(screenCaptureDidChange)
+                                                 name:UIScreen.capturedDidChangeNotification
+                                               object:nil];
+    
+    // 2. Protección contra capturas de pantalla
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(userDidTakeScreenshot)
+                                                 name:UIApplication.userDidTakeScreenshotNotification
+                                               object:nil];
+    
+    // 3. Protección al cambiar de app / multitarea
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(appWillResignActive)
                                                  name:UIApplicationWillResignActiveNotification
@@ -60,23 +76,42 @@
                                                  name:UIApplicationDidBecomeActiveNotification
                                                object:nil];
     
-    // 2. Protección contra grabación de pantalla
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(screenCaptureDidChange)
-                                                 name:UIScreen.capturedDidChangeNotification
-                                               object:nil];
-    
-    // 3. Protección contra capturas de pantalla
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(userDidTakeScreenshot)
-                                                 name:UIApplication.userDidTakeScreenshotNotification
-                                               object:nil];
-    
     // Verificar si ya está grabando al iniciar
     self.isRecording = [UIScreen mainScreen].isCaptured;
     if (self.isRecording) {
         [self showProtection];
     }
+}
+
+#pragma mark - Grabación de pantalla
+
+- (void)screenCaptureDidChange {
+    BOOL isCaptured = [UIScreen mainScreen].isCaptured;
+    
+    if (isCaptured && !self.isRecording) {
+        // Empezó a grabar - poner pantalla negra inmediatamente
+        self.isRecording = YES;
+        [self showProtection];
+    } else if (!isCaptured && self.isRecording) {
+        // Paró de grabar - quitar pantalla negra
+        self.isRecording = NO;
+        [self hideProtection];
+    }
+}
+
+#pragma mark - Captura de pantalla
+
+- (void)userDidTakeScreenshot {
+    // La captura ya se tomó, pero activamos la protección inmediatamente
+    // para que la siguiente captura (si hay) salga negra
+    [self showProtection];
+    
+    // Después de un breve momento, ocultamos (si no está grabando)
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (!self.isRecording) {
+            [self hideProtection];
+        }
+    });
 }
 
 #pragma mark - Cambio de app / Multitarea
@@ -92,58 +127,16 @@
     }
 }
 
-#pragma mark - Grabación de pantalla
-
-- (void)screenCaptureDidChange {
-    BOOL isCaptured = [UIScreen mainScreen].isCaptured;
-    
-    if (isCaptured && !self.isRecording) {
-        // Empezó a grabar
-        self.isRecording = YES;
-        [self showProtection];
-        
-        // Mostrar alerta
-        dispatch_async(dispatch_get_main_queue(), ^{
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Grabación detectada"
-                message:@"La pantalla se ha ocultado por seguridad."
-                preferredStyle:UIAlertControllerStyleAlert];
-            [alert addAction:[UIAlertAction actionWithTitle:@"Entendido" style:UIAlertActionStyleDefault handler:nil]];
-            [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
-        });
-    } else if (!isCaptured && self.isRecording) {
-        // Paró de grabar
-        self.isRecording = NO;
-        [self hideProtection];
-    }
-}
-
-#pragma mark - Captura de pantalla
-
-- (void)userDidTakeScreenshot {
-    // La captura ya se tomó, no se puede prevenir
-    // Pero podemos mostrar una advertencia
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Captura detectada"
-            message:@"Las capturas de pantalla están prohibidas y pueden ser registradas."
-            preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"Entendido" style:UIAlertActionStyleDefault handler:nil]];
-        [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
-    });
-}
-
 #pragma mark - Mostrar/Ocultar protección
 
 - (void)showProtection {
-    [UIView animateWithDuration:0.3 animations:^{
-        self.protectionView.alpha = 1.0;
-    }];
+    // Sin animación para que sea instantáneo
+    self.protectionView.alpha = 1.0;
 }
 
 - (void)hideProtection {
-    [UIView animateWithDuration:0.3 animations:^{
-        self.protectionView.alpha = 0.0;
-    }];
+    // Sin animación para que sea instantáneo
+    self.protectionView.alpha = 0.0;
 }
 
 #pragma mark - Pantalla de licencia
