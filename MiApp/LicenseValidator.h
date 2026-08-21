e4826d41b26450f28c20456b3107ed3f97a1fe5b
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 09:17:31 -0500

    Add asynchronous key validation method

--- MiApp/LicenseValidator.h
+++ MiApp/LicenseValidator.h
@@ -1,8 +1,16 @@
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void (^LicenseValidationCompletion)(BOOL valid,
+                                             NSString * _Nullable reason,
+                                             NSString * _Nullable expiresAt);
+
 @interface LicenseValidator : NSObject
 
-+ (BOOL)isValidKey:(NSString *)key;
-+ (NSArray *)validKeys;
++ (void)validateKey:(NSString *)key
+         completion:(LicenseValidationCompletion)completion;
 
 @end
+
+NS_ASSUME_NONNULL_END
