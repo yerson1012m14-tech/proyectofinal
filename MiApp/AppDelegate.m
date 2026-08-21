29c88f8db441892adf4cc7b987fff4e30605215a
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-20 23:32:01 -0500

    Update AppDelegate.m

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -1,23 +1,9 @@
 #import "AppDelegate.h"
-#import "ViewController.h"
-#import "KeyViewController.h"
-#import "KeyManager.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    
-    UIViewController *rootVC;
-    if ([[KeyManager sharedManager] isKeyValid]) {
-        ViewController *main = [[ViewController alloc] init];
-        rootVC = [[UINavigationController alloc] initWithRootViewController:main];
-    } else {
-        rootVC = [[KeyViewController alloc] init];
-    }
-    
-    self.window.rootViewController = rootVC;
-    [self.window makeKeyAndVisible];
+    // Inicialización de la aplicación
     return YES;
 }
 
