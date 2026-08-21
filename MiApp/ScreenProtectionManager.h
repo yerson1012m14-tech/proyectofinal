df3c42b7fd238a3632b92c41f62c12987b870fac
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-21 07:00:14 -0500

    Refactor ScreenProtectionManager interface
    Removed unused methods and updated shared manager method.

--- MiApp/ScreenProtectionManager.h
+++ MiApp/ScreenProtectionManager.h
@@ -1,16 +1,12 @@
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-
-NS_ASSUME_NONNULL_BEGIN
 
 @interface ScreenProtectionManager : NSObject
 
-+ (instancetype)sharedManager;
++ (instancetype)shared;
 
-@property (nonatomic, assign, getter=isEnabled) BOOL enabled;
-
-- (void)startMonitoring;
-- (void)stopMonitoring;
+- (void)enableProtection;
+- (void)disableProtection;
+- (BOOL)isProtectionEnabled;
 
 @end
-
-NS_ASSUME_NONNULL_END
