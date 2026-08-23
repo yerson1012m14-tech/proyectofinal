74d675d2f3f1c3d67714b4b5beddbb1050064244
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-23 12:00:13 -0500

    Delete SettingsViewController.h

--- SettingsViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface SettingsViewController : UIViewController
-
-@property (nonatomic, assign) NSInteger selectedLanguage;
-@property (nonatomic, assign) BOOL screenProtection;
-@property (nonatomic, copy) void (^onSettingsChanged)(void);
-
-@end
